da4d5289690a8717dda673848c524ab9fcced242
Sean Hanson <smh@seanmhanson.com>
2020-06-22 12:21:21 -0400

    master: PR Feedback, ensure JS and TS parity

--- index.js
+++ index.js
@@ -10,77 +10,263 @@
  * Ported by Bryce Neal.
  */
 
-var statusCodes = {};
-
-statusCodes[exports.ACCEPTED = 202] = "Accepted";
-statusCodes[exports.BAD_GATEWAY = 502] = "Bad Gateway";
-statusCodes[exports.BAD_REQUEST = 400] = "Bad Request";
-statusCodes[exports.CONFLICT = 409] = "Conflict";
-statusCodes[exports.CONTINUE = 100] = "Continue";
-statusCodes[exports.CREATED = 201] = "Created";
-statusCodes[exports.EXPECTATION_FAILED = 417] = "Expectation Failed";
-statusCodes[exports.FAILED_DEPENDENCY  = 424] = "Failed Dependency";
-statusCodes[exports.FORBIDDEN = 403] = "Forbidden";
-statusCodes[exports.GATEWAY_TIMEOUT = 504] = "Gateway Timeout";
-statusCodes[exports.GONE = 410] = "Gone";
-statusCodes[exports.HTTP_VERSION_NOT_SUPPORTED = 505] = "HTTP Version Not Supported";
-statusCodes[exports.IM_A_TEAPOT = 418] = "I'm a teapot";
-statusCodes[exports.INSUFFICIENT_SPACE_ON_RESOURCE = 419] = "Insufficient Space on Resource";
-statusCodes[exports.INSUFFICIENT_STORAGE = 507] = "Insufficient Storage";
-statusCodes[exports.INTERNAL_SERVER_ERROR = 500] = "Server Error";
-statusCodes[exports.LENGTH_REQUIRED = 411] = "Length Required";
-statusCodes[exports.LOCKED = 423] = "Locked";
-statusCodes[exports.METHOD_FAILURE = 420] = "Method Failure";
-statusCodes[exports.METHOD_NOT_ALLOWED = 405] = "Method Not Allowed";
-statusCodes[exports.MOVED_PERMANENTLY = 301] = "Moved Permanently";
-statusCodes[exports.MOVED_TEMPORARILY = 302] = "Moved Temporarily";
-statusCodes[exports.MULTI_STATUS = 207] = "Multi-Status";
-statusCodes[exports.MULTIPLE_CHOICES = 300] = "Multiple Choices";
-statusCodes[exports.NETWORK_AUTHENTICATION_REQUIRED = 511] = "Network Authentication Required";
-statusCodes[exports.NO_CONTENT = 204] = "No Content";
-statusCodes[exports.NON_AUTHORITATIVE_INFORMATION = 203] = "Non Authoritative Information";
-statusCodes[exports.NOT_ACCEPTABLE = 406] = "Not Acceptable";
-statusCodes[exports.NOT_FOUND = 404] = "Not Found";
-statusCodes[exports.NOT_IMPLEMENTED = 501] = "Not Implemented";
-statusCodes[exports.NOT_MODIFIED = 304] = "Not Modified";
-statusCodes[exports.OK = 200] = "OK";
-statusCodes[exports.PARTIAL_CONTENT = 206] = "Partial Content";
-statusCodes[exports.PAYMENT_REQUIRED = 402] = "Payment Required";
-statusCodes[exports.PERMANENT_REDIRECT = 308] = "Permanent Redirect";
-statusCodes[exports.PRECONDITION_FAILED = 412] = "Precondition Failed";
-statusCodes[exports.PRECONDITION_REQUIRED = 428] = "Precondition Required";
-statusCodes[exports.PROCESSING = 102] = "Processing";
-statusCodes[exports.PROXY_AUTHENTICATION_REQUIRED = 407] = "Proxy Authentication Required";
-statusCodes[exports.REQUEST_HEADER_FIELDS_TOO_LARGE = 431] = "Request Header Fields Too Large";
-statusCodes[exports.REQUEST_TIMEOUT = 408] = "Request Timeout";
-statusCodes[exports.REQUEST_TOO_LONG = 413] = "Request Entity Too Large";
-statusCodes[exports.REQUEST_URI_TOO_LONG = 414] = "Request-URI Too Long";
-statusCodes[exports.REQUESTED_RANGE_NOT_SATISFIABLE = 416] = "Requested Range Not Satisfiable";
-statusCodes[exports.RESET_CONTENT = 205] = "Reset Content";
-statusCodes[exports.SEE_OTHER = 303] = "See Other";
-statusCodes[exports.SERVICE_UNAVAILABLE = 503] = "Service Unavailable";
-statusCodes[exports.SWITCHING_PROTOCOLS = 101] = "Switching Protocols";
-statusCodes[exports.TEMPORARY_REDIRECT = 307] = "Temporary Redirect";
-statusCodes[exports.TOO_MANY_REQUESTS = 429] = "Too Many Requests";
-statusCodes[exports.UNAUTHORIZED = 401] = "Unauthorized";
-statusCodes[exports.UNPROCESSABLE_ENTITY = 422] = "Unprocessable Entity";
-statusCodes[exports.UNSUPPORTED_MEDIA_TYPE = 415] = "Unsupported Media Type";
-statusCodes[exports.USE_PROXY = 305] = "Use Proxy";
-
-exports.getStatusText = function(statusCode) {
-  if (statusCodes.hasOwnProperty(statusCode)) {
-    return statusCodes[statusCode];
-  } else {
-    throw new Error("Status code does not exist: " + statusCode);
-  }
+var statusCodeData = {
+  ACCEPTED: {
+    statusCode: 202,
+    reasonPhrase: "Accepted"
+  },
+  BAD_GATEWAY: {
+    statusCode: 502,
+    reasonPhrase: "Bad Gateway"
+  },
+  BAD_REQUEST: {
+    statusCode: 400,
+    reasonPhrase: "Bad Request"
+  },
+  CONFLICT: {
+    statusCode: 409,
+    reasonPhrase: "Conflict"
+  },
+  CONTINUE: {
+    statusCode: 100,
+    reasonPhrase: "Continue"
+  },
+  CREATED: {
+    statusCode: 201,
+    reasonPhrase: "Created"
+  },
+  EXPECTATION_FAILED: {
+    statusCode: 417,
+    reasonPhrase: "Expectation Failed"
+  },
+  FAILED_DEPENDENCY : {
+    statusCode: 424,
+    reasonPhrase: "Failed Dependency"
+  },
+  FORBIDDEN: {
+    statusCode: 403,
+    reasonPhrase: "Forbidden"
+  },
+  GATEWAY_TIMEOUT: {
+    statusCode: 504,
+    reasonPhrase: "Gateway Timeout"
+  },
+  GONE: {
+    statusCode: 410,
+    reasonPhrase: "Gone"
+  },
+  HTTP_VERSION_NOT_SUPPORTED: {
+    statusCode: 505,
+    reasonPhrase: "HTTP Version Not Supported"
+  },
+  IM_A_TEAPOT: {
+    statusCode: 418,
+    reasonPhrase: "I'm a teapot"
+  },
+  INSUFFICIENT_SPACE_ON_RESOURCE: {
+    statusCode: 419,
+    reasonPhrase: "Insufficient Space on Resource"
+  },
+  INSUFFICIENT_STORAGE: {
+    statusCode: 507,
+    reasonPhrase: "Insufficient Storage"
+  },
+  INTERNAL_SERVER_ERROR: {
+    statusCode: 500,
+    reasonPhrase: "Server Error"
+  },
+  LENGTH_REQUIRED: {
+    statusCode: 411,
+    reasonPhrase: "Length Required"
+  },
+  LOCKED: {
+    statusCode: 423,
+    reasonPhrase: "Locked"
+  },
+  METHOD_FAILURE: {
+    statusCode: 420,
+    reasonPhrase: "Method Failure"
+  },
+  METHOD_NOT_ALLOWED: {
+    statusCode: 405,
+    reasonPhrase: "Method Not Allowed"
+  },
+  MOVED_PERMANENTLY: {
+    statusCode: 301,
+    reasonPhrase: "Moved Permanently"
+  },
+  MOVED_TEMPORARILY: {
+    statusCode: 302,
+    reasonPhrase: "Moved Temporarily"
+  },
+  MULTI_STATUS: {
+    statusCode: 207,
+    reasonPhrase: "Multi-Status"
+  },
+  MULTIPLE_CHOICES: {
+    statusCode: 300,
+    reasonPhrase: "Multiple Choices"
+  },
+  NETWORK_AUTHENTICATION_REQUIRED: {
+    statusCode: 511,
+    reasonPhrase: "Network Authentication Required"
+  },
+  NO_CONTENT: {
+    statusCode: 204,
+    reasonPhrase: "No Content"
+  },
+  NON_AUTHORITATIVE_INFORMATION: {
+    statusCode: 203,
+    reasonPhrase: "Non Authoritative Information"
+  },
+  NOT_ACCEPTABLE: {
+    statusCode: 406,
+    reasonPhrase: "Not Acceptable"
+  },
+  NOT_FOUND: {
+    statusCode: 404,
+    reasonPhrase: "Not Found"
+  },
+  NOT_IMPLEMENTED: {
+    statusCode: 501,
+    reasonPhrase: "Not Implemented"
+  },
+  NOT_MODIFIED: {
+    statusCode: 304,
+    reasonPhrase: "Not Modified"
+  },
+  OK: {
+    statusCode: 200,
+    reasonPhrase: "OK"
+  },
+  PARTIAL_CONTENT: {
+    statusCode: 206,
+    reasonPhrase: "Partial Content"
+  },
+  PAYMENT_REQUIRED: {
+    statusCode: 402,
+    reasonPhrase: "Payment Required"
+  },
+  PERMANENT_REDIRECT: {
+    statusCode: 308,
+    reasonPhrase: "Permanent Redirect"
+  },
+  PRECONDITION_FAILED: {
+    statusCode: 412,
+    reasonPhrase: "Precondition Failed"
+  },
+  PRECONDITION_REQUIRED: {
+    statusCode: 428,
+    reasonPhrase: "Precondition Required"
+  },
+  PROCESSING: {
+    statusCode: 102,
+    reasonPhrase: "Processing"
+  },
+  PROXY_AUTHENTICATION_REQUIRED: {
+    statusCode: 407,
+    reasonPhrase: "Proxy Authentication Required"
+  },
+  REQUEST_HEADER_FIELDS_TOO_LARGE: {
+    statusCode: 431,
+    reasonPhrase: "Request Header Fields Too Large"
+  },
+  REQUEST_TIMEOUT: {
+    statusCode: 408,
+    reasonPhrase: "Request Timeout"
+  },
+  REQUEST_TOO_LONG: {
+    statusCode: 413,
+    reasonPhrase: "Request Entity Too Large"
+  },
+  REQUEST_URI_TOO_LONG: {
+    statusCode: 414,
+    reasonPhrase: "Request-URI Too Long"
+  },
+  REQUESTED_RANGE_NOT_SATISFIABLE: {
+    statusCode: 416,
+    reasonPhrase: "Requested Range Not Satisfiable"
+  },
+  RESET_CONTENT: {
+    statusCode: 205,
+    reasonPhrase: "Reset Content"
+  },
+  SEE_OTHER: {
+    statusCode: 303,
+    reasonPhrase: "See Other"
+  },
+  SERVICE_UNAVAILABLE: {
+    statusCode: 503,
+    reasonPhrase: "Service Unavailable"
+  },
+  SWITCHING_PROTOCOLS: {
+    statusCode: 101,
+    reasonPhrase: "Switching Protocols"
+  },
+  TEMPORARY_REDIRECT: {
+    statusCode: 307,
+    reasonPhrase: "Temporary Redirect"
+  },
+  TOO_MANY_REQUESTS: {
+    statusCode: 429,
+    reasonPhrase: "Too Many Requests"
+  },
+  UNAUTHORIZED: {
+    statusCode: 401,
+    reasonPhrase: "Unauthorized"
+  },
+  UNPROCESSABLE_ENTITY: {
+    statusCode: 422,
+    reasonPhrase: "Unprocessable Entity"
+  },
+  UNSUPPORTED_MEDIA_TYPE: {
+    statusCode: 415,
+    reasonPhrase: "Unsupported Media Type"
+  },
+  USE_PROXY: {
+    statusCode: 305,
+    reasonPhrase: "Use Proxy"
+  },
 };
 
+// export a mapping of status name to status code only
+
+var statusCodes = Object.keys(statusCodeData).reduce((acc, statusName) => {
+  acc[statusName] = statusCodeData[statusName].statusCode;
+  return acc;
+}, {});
+
+exports.StatusCodes = statusCodes;
+
+// export each status name individually, set to its status code
+
+Object.keys(statusCodeData).forEach(statusName => {
+  exports[statusName] = statusCodes[statusName];
+});
+
+// helper functions
+
+var statusCodesToReason = Object.keys(statusCodeData).reduce((acc, statusName) => {
+  var status = statusCodeData[statusName];
+  acc[status.statusCode] = status.reasonPhrase;
+  return acc;
+}, {});
+
+exports.getStatusText = function(statusCode) {
+  if (statusCodesToReason.hasOwnProperty(statusCode)) {
+    return statusCodesToReason[statusCode];
+  }
+  throw new Error("Status code does not exist: " + statusCode);
+};
+
+var reasonPhrasesToCode = Object.keys(statusCodesToReason).reduce((acc, statusCode) => {
+  acc[statusCodesToReason[statusCode]] = statusCode;
+  return acc;
+}, {});
+
 exports.getStatusCode = function(reasonPhrase) {
-  for (key in statusCodes) {
-    if (statusCodes[key] === reasonPhrase) {
-      return parseInt(key, 10);
-    }
+  if (reasonPhrasesToCode.hasOwnProperty(reasonPhrase)) {
+    return parseInt(statusCodesToReason[reasonPhrase], 10);
   }
   throw new Error("Reason phrase does not exist: " + reasonPhrase);
 };
-
