74a54782ef97289f1110da46fe92526cf409588c
prettymuchbryce <brycedneal@gmail.com>
2020-08-23 15:50:26 -0700

    Remove unnecessary import/export lines from concatenated definition file

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 tsc --project tsconfig-build.json
 ncc build ./build-tmp/index.js -o ./build
-cat ./build-tmp/codes.d.ts >> ./build-tmp/index.d.ts
-mv ./build-tmp/index.d.ts ./build
+cat ./build-tmp/index.d.ts >> ./build-tmp/codes.d.ts
+# Because we are concatting .d.ts files, we need to remove all imports of codes.ts from index.ts
+sed -i.old '/^import/d' ./build-tmp/codes.d.ts
+# as well as exports that already exist within codes.ts
+sed -i.old '/^export { StatusCodes, ReasonPhrases, }/d;' ./build-tmp/codes.d.ts
+mv ./build-tmp/codes.d.ts ./build/index.d.ts
 rm -rf ./build-tmp
